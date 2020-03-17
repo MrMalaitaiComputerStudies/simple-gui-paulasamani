983403abf30644cdcc7d891353278ec89ff9dfed
mrmalaitai <62281287+mrmalaitai@users.noreply.github.com>
2020-03-18 10:09:29 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple-GUI
+
+Edit "test1.py" and follow the comments and instructions
---
+++ test1.py
@@ -0,0 +1,19 @@
+# Author: 
+# Date: 
+# The purpose of this is to demonstrate the ability to create a simple GUI, commit, and then push it so that Mr Malaitai can read it.
+
+# The GUI is to have the following:
+# 1 Label widget that says "Enter name"
+# 1 Entry widget next to the Label widget
+# 1 Button widget that spans over 2 columns underneath the Label and Entry widgets.
+import tkinter as tk
+
+
+root = tk.Tk()
+
+
+
+
+
+
+root.mainloop()
